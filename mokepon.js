6efab581b34b6fcb49358166f3b432c39fa39eec
Andres Parra <cifucar79@gmail.com>
2023-12-10 14:39:52 -0500

    funcion para ataque aleatorios definida

--- mokepon.js
+++ mokepon.js
@@ -8,16 +8,38 @@ function seleccionarMascotaJugador(){
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
 
     if (inputHipodoge.checked) {
-        alert('seleccionaste a hipodoge');
+        spanMascotaJugador.innerText = 'Hipodoge';
     }else if(inputCapipepo.checked){
-        alert('Seleccionaste a capipepo');
+        spanMascotaJugador.innerText = 'Capipepo';
     }else if(inputRatigueya.checked){
-        alert('Seleccionaste la ratigueya');
+        spanMascotaJugador.innerText = 'Ratigueya';
     }else{
         alert("no me toques ahi");
-    }
+    };
+    seleccionarMascotaEnemigo();
 };
+
+
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3);
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+
+    if(ataqueAleatorio === 1){
+        spanMascotaEnemigo.innerText = "Hipodoge";
+    }else if(ataqueAleatorio === 2){
+        spanMascotaEnemigo.innerText = "Capipepo";
+    }else{
+        spanMascotaEnemigo.innerText = "Ratigueya";
+    };
+};
+
+
+function aleatorio(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 
 window.addEventListener('load',iniciarJuego);
