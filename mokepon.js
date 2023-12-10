7317736bec63f0eb285c6bd318ea66a57240407f
Andres Parra <cifucar79@gmail.com>
2023-12-10 13:32:51 -0500

    Empezamos con la funcion seleccionarMascotaJugador

--- mokepon.js
+++ mokepon.js
@@ -1,18 +1,23 @@
 function iniciarJuego(){
-    
+    let botonMascotaJugador = document.querySelector("#boton-mascota");
+    botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador); 
 };
 
 
 function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota");
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+
+    if (inputHipodoge.checked) {
+        alert('seleccionaste a hipodoge');
+    }else if(inputCapipepo.checked){
+        alert('Seleccionaste a capipepo');
+    }else if(inputRatigueya.checked){
+        alert('Seleccionaste la ratigueya');
+    }else{
+        alert("no me toques ahi");
+    }
 };
 
-
-let botonMascotaJugador = document.querySelector("#boton-mascota");
-botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador);
-
 window.addEventListener('load',iniciarJuego);
-
-
-
-
